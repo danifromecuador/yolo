6c5b3aa0c7117170c81b0a55f11adeff9a7b3540
dani <danifromecuador@gmail.com>
2023-12-18 20:47:51 -0500

    Create Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <div className="App">
+      <Header />
       Let&apos;s create something amazing here!
     </div>
   )
