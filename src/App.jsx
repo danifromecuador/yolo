c0c79c9c699532bbcc6e14497e6d676e58cb1a51
dani <danifromecuador@gmail.com>
2023-12-20 22:24:40 -0500

    Create Components and Layout them

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
+import Footer from './components/Footer'
 import Header from './components/Header'
+import Inputs from './components/Inputs'
+import MainGrid from './components/MainGrid'
+import Outputs from './components/Outputs'
 
 function App() {
 
   return (
     <div className="App">
       <Header />
+      <Inputs />
+      <MainGrid />
+      <Outputs />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Inputs.jsx
@@ -0,0 +1,9 @@
+const Inputs = () => {
+  return (
+    <div className="inputs">
+      <p>Inputs</p>
+    </div>
+  );
+}
+
+export default Inputs;
