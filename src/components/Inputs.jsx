18a52c2daeb4d58d52b5923d9c69ce6fad933b17
dani <danifromecuador@gmail.com>
2023-12-27 21:27:24 -0500

    Edit input fields to calculate the grid

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,9 +1,47 @@
+import { useState } from 'react';
+import '../stylesheets/Inputs.css';
 const Inputs = () => {
+  const [yearOfBirth, setYearOfBirth] = useState("");
+  const [lifeExpectancy, setLifeExpectancy] = useState("");
+
+  const handleYearOfBirth = (e) => {
+    setYearOfBirth(e.target.value);
+  }
+
+  const handleLifeExpectancy = (e) => {
+    setLifeExpectancy(e.target.value);
+  }
+
+  const clearPlaceholder = (e) => {
+    e.target.placeholder = "";
+  }
+
+
+  const resetPlaceholder = (e) => {
+    e.target.placeholder = e.target.name === 'yearOfBirth' ? 'Enter year of birth' : 'Enter life expectancy';
+  }
+
   return (
     <div className="inputs">
-      <input type="text" placeholder="Enter your name" />
-      <input type="text" placeholder="Enter your email" />
-      <input type="text" placeholder="Enter your password" />
+      <input
+        type="number"
+        name="yearOfBirth"
+        placeholder="Enter year of birth"
+        value={yearOfBirth}
+        onChange={handleYearOfBirth}
+        onClick={clearPlaceholder}
+        onBlur={resetPlaceholder}
+      />
+      <input
+        type="number"
+        name="lifeExpectancy"
+        placeholder="Enter life expectancy"
+        value={lifeExpectancy}
+        onChange={handleLifeExpectancy}
+        onClick={clearPlaceholder}
+        onBlur={resetPlaceholder}
+      />
+      <button>Calculate</button>
     </div>
   );
 }
